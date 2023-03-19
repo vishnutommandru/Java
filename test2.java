024a3ccbe8494727b72f2826f5ab2633138d0b9f
vishnutommandru <46985936+vishnutommandru@users.noreply.github.com>
2023-03-18 19:20:54 -0600

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,14 +1,56 @@
-public class ReverseString {
-    public static void main(String[] args) {
-        String originalString = "Vishnu";
-        String reversedString = reverseString(originalString);
+public class LinkedList {
+    Node head; // head of the linked list
 
-        System.out.println("Original string: " + originalString);
-        System.out.println("Reversed string: " + reversedString);
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
     }
 
-    public static String reverseString(String str) {
-        StringBuilder sb = new StringBuilder(str);
-        return sb.reverse().toString();
+    // Method to reverse the linked list
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    // Method to print the linked list
+    public void printList() {
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new Node(25);
+        list.head.next = new Node(78);
+        list.head.next.next = new Node(56);
+        list.head.next.next.next = new Node(34);
+        list.head.next.next.next.next = new Node(56);
+        list.head.next.next.next.next.next = new Node(78);
+        list.head.next.next.next.next.next.next = new Node(90);
+
+        System.out.println("Original Linked List:");
+        list.printList();
+
+        list.reverse();
+
+        System.out.println("Reversed Linked List:");
+        list.printList();
     }
 }
