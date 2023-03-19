8d78b9f21e04834c8839e88b4b4b0089165a53de
vishnutommandru <46985936+vishnutommandru@users.noreply.github.com>
2023-03-18 18:59:12 -0600

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,5 @@
-String originalStr = "Hello";
-String reversedStr = "";
-
-for (int i = 0; i < originalStr.length(); i++) {
-  reversedStr = originalStr.charAt(i) + reversedStr;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
-
-System.out.println("Reversed string: "+ reversedStr);
