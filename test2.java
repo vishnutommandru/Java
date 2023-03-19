9acc8e62b9d535601b08b3a8e363fe053708dc33
vishnutommandru <46985936+vishnutommandru@users.noreply.github.com>
2023-03-18 19:23:16 -0600

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,56 +1,21 @@
-public class LinkedList {
-    Node head; // head of the linked list
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-    static class Node {
-        int data;
-        Node next;
+public class HelloServlet extends HttpServlet {
 
-        Node(int d) {
-            data = d;
-            next = null;
-        }
-    }
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
 
-    // Method to reverse the linked list
-    public void reverse() {
-        Node prev = null;
-        Node current = head;
-        Node next = null;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        head = prev;
-    }
-
-    // Method to print the linked list
-    public void printList() {
-        Node node = head;
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.head = new Node(25);
-        list.head.next = new Node(78);
-        list.head.next.next = new Node(56);
-        list.head.next.next.next = new Node(34);
-        list.head.next.next.next.next = new Node(56);
-        list.head.next.next.next.next.next = new Node(78);
-        list.head.next.next.next.next.next.next = new Node(90);
-
-        System.out.println("Original Linked List:");
-        list.printList();
-
-        list.reverse();
-
-        System.out.println("Reversed Linked List:");
-        list.printList();
-    }
+    PrintWriter out = response.getWriter();
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Hello, World!</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>Hello, World!</h1>");
+    out.println("<p>This is a sample Java website.</p>");
+    out.println("</body>");
+    out.println("</html>");
+  }
 }
